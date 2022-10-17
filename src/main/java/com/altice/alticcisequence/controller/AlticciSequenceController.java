e61e488b2ba09728e846e1039e7c6853d444394b
wienerdev <matheuswiener9@gmail.com>
2022-10-17 10:16:11 -0300

    removendo import desnecessario

--- src/main/java/com/altice/alticcisequence/controller/AlticciSequenceController.java
+++ src/main/java/com/altice/alticcisequence/controller/AlticciSequenceController.java
@@ -2,7 +2,6 @@ package com.altice.alticcisequence.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
